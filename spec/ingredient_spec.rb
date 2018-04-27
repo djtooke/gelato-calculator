ea89250705c5ea14f7404bbddb30895145dcb3c7
Daniel Tooke <daniel.tooke@gmail.com>
2018-04-27 17:24:03 +0100

    added Gorgonzola as subject in RSPEC

--- spec/ingredient_spec.rb
+++ spec/ingredient_spec.rb
@@ -1,8 +1,11 @@
 require 'ingredient'
 
 describe Ingredient do
+
+  subject(:gorgonzola) {Ingredient.new(28, 0, 20, 2, 50)}
+
   it 'should have an accessible hash with its constituent percentages' do
-    gorgonzola = Ingredient.new(28, 0, 20, 2, 50)
     expect(gorgonzola).to respond_to (:percentage)
   end
+
 end
