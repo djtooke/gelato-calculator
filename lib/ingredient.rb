57439276789d38d00139185273b91e09e9479c66
Daniel Tooke <daniel.tooke@gmail.com>
2018-04-27 17:07:34 +0100

    Added percentage hash to ingredients

--- lib/ingredient.rb
+++ lib/ingredient.rb
@@ -1,2 +1,15 @@
 class Ingredient
+
+  attr_reader :percentage
+
+  def initialize(fat, sugar, lean_milk_solids, other_solids, water)
+    @percentage = {
+      fat: fat,
+      sugar: sugar,
+      lean_milk_solids: lean_milk_solids,
+      other_solids: other_solids,
+      water: water
+    }
+    end
+
 end
