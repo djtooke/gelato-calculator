29bf2e3397215aebc0229e0a715ff106dd38875b
Daniel Tooke <daniel.tooke@gmail.com>
2018-04-30 22:51:35 +0100

    Refactored ingredient checking to add private method

--- lib/gelato.rb
+++ lib/gelato.rb
@@ -7,9 +7,15 @@ class Gelato
   end
 
   def add_ingredient(ingredient, grams)
+    check_ingredients(ingredient, grams)
+    @ingredients.store(ingredient, grams)
+  end
+
+  private
+
+  def check_ingredients(ingredient, grams)
     raise 'Weight in grams must be an integer.' unless grams.class == Integer
     raise 'Ingredient must be an ingredient object.' unless ingredient.class == Ingredient
-    @ingredients.store(ingredient, grams)
   end
 
 end
