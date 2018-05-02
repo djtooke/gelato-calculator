fe9c56710c3817aa2f0bcadc2a74f49f006a49f0
Daniel Tooke <daniel.tooke@gmail.com>
2018-05-02 12:12:19 +0100

    Added scalar function to calculator and test

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,10 +1,19 @@
 class Calculator
 
-  attr_reader :gelato
+  attr_reader :gelato, :scalar
 
   def initialize(gelato)
     raise "#{gelato} is not a Gelato object." unless gelato.class == Gelato
     @gelato = gelato
+    @scalar = scale.round(3)
+  end
+
+private
+
+  def scale
+    total = 0
+    @gelato.ingredients.each_value { |weight| total += weight }
+    100.0 / total
   end
 
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -5,8 +5,19 @@ describe Calculator do
   let(:gorgonzola_and_honey) { double :gelato}
   subject (:calc) {Calculator.new(gorgonzola_and_honey)}
 
+  # May need to create mock ingredient objects and work out how to put them as the hash keys
   before(:example) do
     allow(gorgonzola_and_honey).to receive(:class).and_return(Gelato)
+    allow(gorgonzola_and_honey).to receive(:ingredients).and_return({
+       :milk => 1000,
+       :gorgonzola => 280,
+       :honey => 100,
+       :sucrose => 190,
+       :dextrose =>50,
+       :lmp => 40,
+       :stabilisers => 4,
+       :cream => 220
+     })
   end
 
   it 'should raise an error if initialized with a non-gelato object' do
@@ -15,6 +26,10 @@ describe Calculator do
 
   it 'should store the gelato argument as an instance variable' do
     expect(calc.gelato).to eq(gorgonzola_and_honey)
+  end
+
+  it 'should work out the scalar of the ingredient weights' do
+    expect(calc.scalar).to eq(0.053)
   end
 
 end
